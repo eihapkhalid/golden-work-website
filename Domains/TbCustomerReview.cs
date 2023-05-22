00545c30c9aea7e0073422467ebc6e77a4d4e5f0
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 14:23:05 +0300

    modified:   Domains/TbCustomerReview.cs

--- Domains/TbCustomerReview.cs
+++ Domains/TbCustomerReview.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Domains
 {
-    internal class TbCustomerReview
+    public class TbCustomerReview
     {
+        public int CustomerReviewID { get; set; }
+        public string CustomerID { get; set;}
+        public string CustomerReviewText { get; set;}
+        public int CustomerReviewRating { get; set;}
+        public string CustomerReviewLogo { get; set;}
+        public DateTime UserUpdateTime { get; set; }
+        public DateTime UserCreateTime { get; set; }
+        public int CustomerReviewCurrentlyFeatured { get; set;}
+        public int CustomerReviewCurrentState { get; set; }
     }
 }
