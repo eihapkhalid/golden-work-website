005d85fd191c8ff3c489f22a9c81eb00627e8117
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 13:35:29 +0300

    add coulmns TbService

---
+++ Domains/TbService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains
+{
+    public class TbService
+    {
+        public int ServiceID { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceTitle { get; set; }
+        public string ServiceDescription { get; set; }
+        public decimal ServicePrice { get; set; }
+        public string ServiceImage { get; set;}
+        public DateTime UserUpdateTime { get; set; }
+        public DateTime UserCreateTime { get; set; }
+        public string ServiceCurrentlyFeatured { get; set;}
+        public int ServiceCurrentState { get; set; }
+
+    }
+}
