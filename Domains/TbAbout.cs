eeb9bbe0c3c4b0370b910f4bcf0559eb3ef505ff
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 15:29:59 +0300

    add relation between TbUser and TbAbout

--- Domains/TbAbout.cs
+++ Domains/TbAbout.cs
@@ -8,5 +8,29 @@ namespace Domains
 {
     public class TbAbout
     {
+        public int AboutID { get; set; }
+        public string AboutCompanyName { get; set; }
+        public string AboutCompanyTitle { get; set; }
+        public string AboutFeature { get; set; }
+        public string AboutCompanyDescription { get; set;}
+        public string AboutCompanyAddress { get; set;}
+        public int AboutCompanyPhone { get;set;}
+        public string AboutCompanyEmail { get;set;}
+        public string AboutCompanyLogo { get;set;}
+        public string AboutCompanyHistory { get; set; }
+        public string AboutCompanyValues { get; set; }
+        public string AboutCompanyAchievements { get; set; }
+        public int AboutCompletedProjects { get; set; }
+        public int AboutYearsExperience { get;set; }
+        public string AboutWorkingTime { get; set; }
+        public string AboutWorkingDays { get; set; }
+        public DateTime UserUpdateTime { get; set; }
+        public DateTime UserCreateTime { get; set; }
+        public int AboutCurrentState { get; set; }
+
+        //TbAbout has only user (Update Or Create):
+        public TbUser UpdatedByUser { get; set; }
+        public TbUser CreatedByUser { get; set; }
+        
     }
 }
