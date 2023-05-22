e55c6df9956991be7aeaa1cb439f2ddee39b3cc2
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 13:32:12 +0300

    modified:   Domains/TbCustomer.cs

--- Domains/TbCustomer.cs
+++ Domains/TbCustomer.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Domains
 {
-    internal class TbCustomer
+    public class TbCustomer
     {
+        public int CustomerID { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CustomerEmail { get; set;}
+        public int CustomerPhone { get; set;}
+        public string CustomerAddress { get; set;}
+        public string CustomerSatisfied { get; set;}
+        public DateTime UserUpdateTime { get;set;}
+        public DateTime UserCreateTime { get; set;}
+        public int CustomerCurrentState { get; set;}
     }
 }
