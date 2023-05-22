1f43c6c8e5ea0429bd43cf24a1348eaa41b5e802
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 12:18:34 +0300

    firest repo

---
+++ GoldenWorkWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoldenWorkWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
