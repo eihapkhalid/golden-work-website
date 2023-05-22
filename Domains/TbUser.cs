0b211642b535901716baec25f858687613c99d3a
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 15:44:00 +0300

    update Tbuser with relations between all tables

--- Domains/TbUser.cs
+++ Domains/TbUser.cs
@@ -18,5 +18,30 @@ namespace Domains
         public DateTime UserUpdateTime { get; set; }
         public DateTime UserCreateTime { get; set; }
         public int UserCurrentState { get; set; }
+
+        //many of users have many proccecs for (Updating Or Creating):
+        public ICollection<TbAbout> AboutUpdatedByUser { get; set; }
+        public ICollection<TbAbout> AboutCreatedByUser { get; set; }
+
+        public ICollection<TbBooking> BookingUpdatedByUser { get; set; }
+        public ICollection<TbBooking> BookingCreatedByUser { get; set; }
+
+        public ICollection<TbContact> ContactUpdatedByUser { get; set; }
+        public ICollection<TbContact> ContactCreatedByUser { get; set; }
+
+        public ICollection<TbCustomer> CustomerUpdatedByUser { get; set; }
+        public ICollection<TbCustomer> CustomerCreatedByUser { get; set; }
+
+        public ICollection<TbCustomerReview> CustomerReviewUpdatedByUser { get; set; }
+        public ICollection<TbCustomerReview> CustomerReviewCreatedByUser { get; set; }
+
+        public ICollection<TbNews> NewsUpdatedByUser { get; set; }
+        public ICollection<TbNews> NewsCreatedByUser { get; set; }
+
+        public ICollection<TbService> ServiceUpdatedByUser { get; set; }
+        public ICollection<TbService> ServiceCreatedByUser { get; set; }
+
+        public ICollection<TbTechnician> TechnicianUpdatedByUser { get; set; }
+        public ICollection<TbTechnician> TechnicianCreatedByUser { get; set; }
     }
 }
