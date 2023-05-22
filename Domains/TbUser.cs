71e4aa0bf6d28aadf7285e8c01f59310d1603e83
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 16:10:40 +0300

    add validations to TbUser

--- Domains/TbUser.cs
+++ Domains/TbUser.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace Domains
 {
     public class TbUser
     {
+        [Key]
         public int UserID { get; set; }
+        [Required(ErrorMessage = "User Name is Required")]
+        [StringLength(50)]
         public string UserName { get; set; }
+        [Required (ErrorMessage = "Password is Required")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 20 characters.")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{8,20}$")]
         public string UserPassword { get; set; }
+        [Required (ErrorMessage ="Email is Required")]
+        [StringLength(50, ErrorMessage ="Length must be less than 50")]
         public string UserEmail { get; set; }
+        [Required(ErrorMessage = "User Role is Required")]
+        [StringLength(50, ErrorMessage ="length must be less than 50")]
         public string UserRole { get; set; }
+        [ValidateNever]
         public int UserUpdate { get; set; }
+        [ValidateNever]
         public int UserCreate { get; set; }
         public DateTime UserUpdateTime { get; set; }
         public DateTime UserCreateTime { get; set; }
