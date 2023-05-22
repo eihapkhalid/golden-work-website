74d400ab7edef87bac9fa38ce8dd725139f7aa29
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 14:03:20 +0300

    modified:   Domains/TbNews.cs

--- Domains/TbNews.cs
+++ Domains/TbNews.cs
@@ -8,5 +8,13 @@ namespace Domains
 {
     public class TbNews
     {
+        public int NewsID { get; set; }
+        public string NewsTitle { get; set; }
+        public string NewsContent { get; set; }
+        public DateTime NewsDate { get; set; }
+        public DateTime UserUpdateTime { get; set; }
+        public DateTime UserCreateTime { get; set; }
+        public int NewsCurrentState { get; set; }
+        public int NewsCurrentlyFeatured { get; set; }
     }
 }
