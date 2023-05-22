3708d5f4edcb05d47f9b3b309768c0f68458807c
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 14:16:59 +0300

    modified:   Domains/TbContact.cs

--- Domains/TbContact.cs
+++ Domains/TbContact.cs
@@ -8,5 +8,13 @@ namespace Domains
 {
     public class TbContact
     {
+        public int ContactID { get; set; }
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public int ContactPhone { get; set; }
+        public string ContactMessage { get; set; }
+        public DateTime UserUpdateTime { get; set; }
+        public DateTime UserCreateTime { get; set; }
+        public int ContactCurrentState { get; set; }
     }
 }
