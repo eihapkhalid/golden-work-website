290b2f9b3cf56c070e36d881537de4bfb5d6936f
eihapKhalid <eihapinfo@gmail.com>
2023-05-22 15:40:16 +0300

    make relation between TbUser and TbNews

--- Domains/TbNews.cs
+++ Domains/TbNews.cs
@@ -16,5 +16,9 @@ namespace Domains
         public DateTime UserCreateTime { get; set; }
         public int NewsCurrentState { get; set; }
         public int NewsCurrentlyFeatured { get; set; }
+
+        //TbNews has only user (Update Or Create):
+        public TbUser UpdatedByUser { get; set; }
+        public TbUser CreatedByUser { get; set; }
     }
 }
