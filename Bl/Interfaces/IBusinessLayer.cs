9f1baea6f4cf25d8f277413c626b2c2d1994c061
Eihap Khalid <Eihapinfo@gmail.com>
2023-05-23 03:52:17 +0300

    update interface folder

---
+++ Bl/Interfaces/IBusinessLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.Interfaces
+{
+    public interface IBusinessLayer<t>
+    {
+        List<t> GetAll();
+        t GetById(int id);
+        bool Save(t table);
+        bool Delete(int id);
+
+        //t AuthorizeUser(t table);
+
+    }
+}
