5d0b830426cd2aaca7c5ab42a5417b3c7cff4585
Eihap Khalid <Eihapinfo@gmail.com>
2023-05-23 04:05:57 +0300

    inject dbcontext in userService.cs

--- Bl/Services/UserService.cs
+++ Bl/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bl.Interfaces;
+using Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,35 @@ using System.Threading.Tasks;
 
 namespace Bl.Services
 {
-    public class UserService
+    public class UserService : IBusinessLayer<TbUser>
     {
+        #region define DbContext
+        private GoldenWorkDbContext context;
+        private readonly IUnitOfWork unitOfWork;
+        public UserService(GoldenWorkDbContext ctx, IUnitOfWork _unitOfWork)
+        {
+            context = ctx;
+            unitOfWork = _unitOfWork;
+        }
+        #endregion
+        bool IBusinessLayer<TbUser>.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<TbUser> IBusinessLayer<TbUser>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        TbUser IBusinessLayer<TbUser>.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IBusinessLayer<TbUser>.Save(TbUser table)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
